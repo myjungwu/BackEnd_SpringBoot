476d09d043ff2199071691574c4db121959945ba
myjungwu <baek.myungsuk@gmail.com>
2024-08-12 16:05:16 +0900

    V2__inserttable.sql add

--- src/main/java/com/restapi/emp/runner/EmpDepInsertRunner.java
+++ src/main/java/com/restapi/emp/runner/EmpDepInsertRunner.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@Profile("prod")  //InsertRunner 가 동작하지 않도록 하려면 현재 Profile과 다른 값을 주면 됨
+@Profile("local")  //InsertRunner 가 동작하지 않도록 하려면 현재 Profile과 다른 값을 주면 됨
 @RequiredArgsConstructor
 public class EmpDepInsertRunner implements ApplicationRunner {
     final DepartmentRepository departmentRepository;
---
+++ src/main/resources/db/migration/mariadb/V2__insert_department.sql
@@ -0,0 +1,9 @@
+insert into departments (department_name,department_description) values
+('HR','performs human resource management functions'),
+('Marketing','creates strategies for selling its company products'),
+('Sales','identifies sales goals and objectives and prepares a sales plan');
+
+insert into employees (email_id,first_name,last_name,department_id) values
+('John@company.com','John','Smith',1),
+('Sarah@company.com','Sarah','Johnson',2),
+('Emilyh@company.com','Emily','Brown',3);
